a074aa79f9e116b66efc3412add9aff3462ccfb8
楊秋会 <yangqiuhui@legendapl.com.cn>
2020-05-22 09:55:37 +0800

    add primary datasource

--- src/main/java/com/yousyuukai/presto/api/config/DataSourceConfig.java
+++ src/main/java/com/yousyuukai/presto/api/config/DataSourceConfig.java
@@ -6,12 +6,20 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+
+    @Primary
+    @Bean(name = "dataSource")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     @Bean(name = "prestoDataSource")
     @ConfigurationProperties(prefix = "presto.datasource")
